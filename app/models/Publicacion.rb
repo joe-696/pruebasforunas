256688715a30288b002f30b935c808a394958254
jefferson joe mendoza vega <joe.mendoza.vega@gmail.com>
2024-04-13 15:13:46 -0500

    prueba forunasss

---
+++ app/models/Publicacion.rb
@@ -0,0 +1,20 @@
+class Publicacion < ApplicationRecord
+    has_one_attached :imagen
+
+    validates :titulo, presence: true 
+    validates :descripcion, presence: true 
+    
+    
+    def self.search(query)
+        where("titulo LIKE ? OR descripcion LIKE ?", "%#{query}%", "%#{query}%")
+    end
+    has_many :comments, dependent: :destroy
+    has_many :notifications, dependent: :destroy
+    has_many :reactions, dependent: :destroy
+    # publicación puede tener muchos comentarios. 
+
+    belongs_to :category
+    belongs_to :user, default: -> {Current.user}
+
+end
+
